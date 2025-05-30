d22928d06a0301e9244d76ddf6a8cbadaaaffc38
Animesh-Jantwal <animeshjantwalsocialmedia@gmail.com>
2025-05-30 17:47:03 +0530

    Lecture 07 notes

---
+++ Codes/Lecture07.js
@@ -0,0 +1,34 @@
+let value = 3;
+let negvalue = -value;
+console.log(negvalue); // -3
+
+/* 
+2+2
+2-2
+2*2
+2**2
+2/3
+2%3
+*/
+
+let str1 = "Hello";
+let str2 = " Animesh";
+
+console.log(str1+str2); //Hellp Animesh
+
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+console.log("1" + 2 + 2 ); //122
+console.log(1 + 2 + "3"); //33
+console.log(true); //true
+console.log(+true); //1
+console.log(+""); //0
+
+
+// Dont write this type of confusion code this is not a good practise for a good dev . Also Don't write tricky and complex code, write simple and readable code so that everyone can read it easily 
+//prefix first updates adn then used and vide-versa in postfix 
+
+
+
+
