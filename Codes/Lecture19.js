0c4078a749f55c4c63c2d255ad0a401d9221957f
Animesh-Jantwal <animeshjantwalsocialmedia@gmail.com>
2025-06-02 14:27:12 +0530

    Lecture 19 notes

---
+++ Codes/Lecture19.js
@@ -0,0 +1,35 @@
+function sayMyName (){  // Format to write a fucntion 
+    console.log("a");
+    console.log("n");
+    console.log("i");
+    console.log("m");
+    console.log("e");
+    console.log("s");
+    console.log("h");
+}
+
+sayMyName(); // Synatx to call a fucntion
+
+                        //Parameters
+function addTwoNumber(number1, number2) {
+    console.log(number1+number2);
+    
+}
+
+addTwoNumber(3,null); //Arguments
+
+
+function add(number1 , number2) {
+    let result = number1+number2;
+    return result;
+}
+const result = add(4,2);
+console.log(result); 
+
+// Must know there is a difference in a fucntion printing something and a function returning some thing 
+// Must know there is a difference in calling a fucntion and print out a function
+
+//(number =1)  // giving default value to a parameter
+
+
+
