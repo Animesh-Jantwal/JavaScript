d9537db5b5541a3891f84407d0e9cbe3054761c9
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 19:37:43 +0530

    UPDATED

---
+++ Codes/LECTURE10.js
@@ -0,0 +1,5 @@
+// 1-> Stack = Used in primitive data types (passed by values)
+
+// 2->Heaps = Used in non-primitive data types (passed by refrence) 
+
+
--- Codes/Lecture10.js
+++
@@ -1,5 +0,0 @@
-// Stack = Used in primitive data types (passed by values)
-
-//Heaps = Used in non-primitive data types (passed by refrence) 
-
-
