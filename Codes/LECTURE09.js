94154e93813f6c6e505b851f4cdfff9c8a481fca
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 19:28:17 +0530

    UPDATED

---
+++ Codes/LECTURE09.js
@@ -0,0 +1,20 @@
+// 1-Primitive data types [are call by values]
+//   strings 
+//   booelan 
+//   number 
+//   null
+//   undefined
+//   symbol
+//   bigInt
+
+// 2-> Non-Primitive/Refrence data types [are call by refrence]
+//     Arrays 
+//     Objects 
+//     Functions 
+
+// 3->javascript is dynamically typed
+
+// 4-> const id1 = Symbol("123");
+//     const id2 = Symbol("123");
+//     console.log(id1 === id2); // false because of symbol data type 
+//     const code = 1232141n;  // This n at the last of number turns it into bigInt
