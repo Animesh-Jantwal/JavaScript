23f7e4d418ac8b561d89296df5147d730bbde2f1
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 18:58:41 +0530

    UPDATED

---
+++ Codes/LECTURE05.js
@@ -0,0 +1,17 @@
+// 1-> "use strict" => To treat all javascript code as newer version 
+// 2-> alert("hello"); => Gives pop up on browser console or after binding in html file but in node it has a different syntax
+// 3-> use official ecma script javascript official documentation and mdn web docs for research 
+
+// 4-> Types of data types 
+
+//     number = Have a defined max and min value 
+//     bigInt = To store ver big number 
+//     string
+//     boolean 
+//     null = standalone value
+//     undefined = When value is not assigned
+//     symbol = To find uniqueness
+
+// 5-> console.log(typeof(a)); => To get the data type of any given input 
+
+// 6-> null => object data type
