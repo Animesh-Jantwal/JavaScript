6d185e1a820dcfecd67db2a0a2401c453739faa2
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 19:22:34 +0530

    UPDATED

---
+++ Codes/LECTURE07.js
@@ -0,0 +1,31 @@
+// 1-> let value = 3;
+//     let negvalue = -value;
+//     console.log(negvalue); // -3
+
+// 2-> Operations 
+//     2+2
+//     2-2
+//     2*2
+//     2**2
+//     2/3
+//     2%3
+
+
+// 3-> let str1 = "Hello";
+//     let str2 = " Animesh";
+//     console.log(str1+str2); //Hell0 Animesh
+
+
+// 3-> console.log("1" + 2); //12
+//     console.log(1 + "2"); //12
+//     console.log("1" + 2 + 2 ); //122
+//     console.log(1 + 2 + "3"); //33
+//     console.log(true); //true
+//     console.log(+true); //1
+//     console.log(+""); //0
+
+// 4-> prefix first updates and then used and vide-versa in postfix 
+
+
+
+
