500e206bc13d6ea67cf670dfd18a757ea975bd33
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 18:12:51 +0530

    UPDATED

---
+++ Codes/LECTURE04.js
@@ -0,0 +1,17 @@
+// 1-> let , var => Can be changed in future 
+//     constant => Can't be changed in future
+
+// 2-> // OR '/' + 'cmd' => To comment the code 
+
+// 3-> console.log([accountID, name , heros]); => To print all the variables and constant at once 
+
+// 4-> var => global variable  
+//     let => block variable
+
+// 4-> let account; // Undefined 
+
+// 5-> /*
+//       Used for mult-line comment 
+//     */
+
+// 6-> Dont use var because for block scope and fucntional scope
