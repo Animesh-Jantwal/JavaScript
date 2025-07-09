4ae51077a40c44353ff11c5e4bebf97036ba5f7e
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-09 19:05:43 +0530

    UPDATED

---
+++ Codes/LECTURE06.js
@@ -0,0 +1,16 @@
+// 1-> let score = 33;
+//     let check  = Number(score); // Used to convert any passsed var/const into Number data type we can also convert into other data types such as string,boolean etc.
+
+// 2-> "33" => 33
+//     "33abc" => NaN
+//     true => 1 and  false => 0
+
+// 3-> let x = 1;
+//     let a = Boolean(x);
+//     console.log(a);
+//     1 => true , 0=> false , "" => false
+
+// 4-> let y = 33;
+//     let b = String(y);
+//     console.log(b); 
+//     33 => "33"
