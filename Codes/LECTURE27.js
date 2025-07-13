92458307dc324e8c429b5b295498898f6eb6bfac
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-13 21:22:30 +0530

    UPDATED

--- Codes/LECTURE27.js
+++ Codes/LECTURE27.js
@@ -1,5 +1,4 @@
 /* 
-
 1-> Basic syntax of for loop
     for(let i=0;i<10;i++){
         console.log(i);
@@ -9,5 +8,4 @@
 2-> break = if written then it break out of the loop 
 
 3-> continue = if written then it continues by skipping that iteration 
-
 */
