5478cb630b21f0fd1848a1813b5a706ad4fcbd3c
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-16 01:06:23 +0530

    UPDATED

---
+++ Codes/LECTURE29.js
@@ -0,0 +1,60 @@
+/*
+1->  Basic Syntax of for of loop
+    for (const num of arr) {           
+    console.log(num);
+    
+     }
+
+2-> const map = new Map()
+    map.set('In' , "India")
+
+    Holds key pair value 
+    Have unique value 
+    Stored  order wise
+
+3-> Maps are iterable but objects are not.
+
+4-> Basic syantax of for in loop ( we can now run a loop on objects)
+    for (const key in object) {
+    console.log(keys);
+    console.log(objects(keys));
+    }
+
+5-> For in loop gives keys of array during output whereas other loops give values.
+
+6-> Map is not iterable using for loop.
+
+7-> Basic syntax of call back function
+
+    const coding = ['js' , 'rust']
+
+    coding.forEach( function (item) {
+    Body of the funciton 
+    } )
+
+    coding.forEach( (item) => {
+        
+        Body of fucntion
+
+    })
+
+    fucntion printMe(item){
+    console.log(item);
+    }
+
+    coding.forEach(printMe)
+
+8-> coding.forEach( (item , index , arr) => {
+    
+    
+    })
+
+9-> Mycoding.forEach( (item) => {   Syntax to access the objects inside array stuff
+    
+    console.log(item,---) 
+    
+    })
+
+
+*/
+
