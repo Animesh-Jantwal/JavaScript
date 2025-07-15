b4f683d60d353b2bb50a0a63308444219d5b178f
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-16 00:46:54 +0530

    Add files via upload

---
+++ Codes/LECTURE28.js
@@ -0,0 +1,14 @@
+/* 
+1-> Basic while loop syntax
+
+    while (condition) {
+        Body ( here we can write the code or body and the updation condition . Can also initilize the variable here.)
+    }
+
+2-> do{
+
+    Body ( here we have to write the code or body and the updation condition. Can also initilize the variable here.)
+    ( Also no matter what , atleast the works gets exectuted for the one time)
+
+    }while(condition)
+*/
