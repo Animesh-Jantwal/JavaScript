613024421d5c5d27041855295429d479dc900497
Animesh_Jantwal <animeshjantwalcoding@gmail.com>
2025-07-23 04:36:56 +0530

    Add files via upload

---
+++ Codes/LECTURE30.js
@@ -0,0 +1,21 @@
+/**  
+
+1-> DOM = Document object model.
+
+2-> console.log(window.document) or (document)
+
+3-> console.log(window) or console.dir(document)
+
+4-> DOM Diagram of Basic HTML Code
+
+    MAKE THE DIAGRAM
+
+5-> document.----- ]-> many stuff
+
+6-> document.getElementbyID(--Write ID here--)
+    This will give you the whole element as an output
+
+7-> document.getElementbyID(--Write ID here--).(--fxn--)
+    can use the above stuff to modify DOM
+
+**/
